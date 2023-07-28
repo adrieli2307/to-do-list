f77cb4cacc5ab1b1e9919f5ff83fbab23b9f8524
tu_nombre_de_usuario <tu_direccion_de_correo>
2023-07-28 12:39:31 -0500

    Button functionality: add and delete

--- to-do-list/src/app/list-of-task/list-of-task.component.ts
+++ to-do-list/src/app/list-of-task/list-of-task.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from '../Interface/task.interface';
 
 @Component({
   selector: 'app-list-of-task',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-of-task.component.css']
 })
 export class ListOfTaskComponent {
+  tasks: Task[] = [];
+  newTask: string = '';
+  errorTask: string = '';
+  taskPendding: Task[] = [];
+  taskCompleted: Task[] = [];
 
+  ngOnInit() {
+    this.loadLocalStorage();
+  }
+
+  addTask() {
+    if (this.newTask.trim() === '') {
+      this.errorTask = 'Por favor, ingresa los datos de la nueva tarea';
+      setTimeout(() => {
+        this.errorTask = ''; // Borra el mensaje de error después de 3 segundos
+      }, 1000);
+      return;
+    }
+    this.errorTask = '';
+    this.tasks.push({ task: this.newTask, completed: false });
+    this.newTask = '';
+    this.savedLocalStorage();
+  }
+
+  savedLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  loadLocalStorage() {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      this.tasks = JSON.parse(savedTasks);
+    }
+  }
+
+  onTaskChange(task: Task) {
+    this.savedLocalStorage();
+  }
+
+  deleteTask(task: Task) {
+    const index = this.tasks.indexOf(task);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+      this.savedLocalStorage();
+    }
+  }
 }
+
+
+
